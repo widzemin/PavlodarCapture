c894021a30fa907025b7d507b40f94ad72bd7176
Ch0p1k3 <Ch00p.228@gmail.com>
2020-07-04 20:19:50 +0600

    Vanya

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-name = 'Maxim'
-print(f'{name} - pidor')
+import telebot
+bot = telebot.TeleBot('1066033330:AAGRLKs4XXRxpY-_GTcls0kYnutiLuEQtrw')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Иди нахуй Максим')
+
+bot.polling()
+
