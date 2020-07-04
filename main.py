4953de2861e565643f7ff04a27bc719f3dd991ae
Maxim <maxim.bassalayev@gmail.com>
2020-07-04 20:26:46 +0600

    добавил

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-name = 'Maxim'
-print(f'{name} - pidor')
+import telebot;
+bot = telebot.TeleBot('1066033330:AAGRLKs4XXRxpY-_GTcls0kYnutiLuEQtrw')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'привет')
+
+@bot.message_handler(commands=['end'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'пошел нахуй')
+
+bot.polling()
